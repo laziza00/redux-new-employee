333d991fd390751774282572af47f52d562b5c4a
laziza@mail.ru <laziza00>
2022-08-05 14:12:38 +0500

    initial commit

---
+++ src/Object/Object.jsx
@@ -0,0 +1,18 @@
+const initialState = [
+  {
+    id: 1,
+    name: 'Husan',
+    email: 'qwerty@gmail.com',
+    number: 2345109876,
+    departament: 'Development',
+  },
+  {
+    id: 2,
+    name: 'Ali',
+    email: 'ytrewq@gmail.com',
+    number: 8765432109,
+    departament: 'Marketing',
+  },
+]
+
+export default initialState
